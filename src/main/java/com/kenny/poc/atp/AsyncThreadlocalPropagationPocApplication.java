b340963c2299787184c71ff7dfe6d51ba6c7dcab
kenny-kerson <kenny.kerson@gmail.com>
2023-11-19 10:59:09 +0900

    Initial Commitˆ

---
+++ src/main/java/com/kenny/poc/atp/AsyncThreadlocalPropagationPocApplication.java
@@ -0,0 +1,13 @@
+package com.kenny.poc.atp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncThreadlocalPropagationPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncThreadlocalPropagationPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kenny/poc/atp/AsyncThreadlocalPropagationPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenny.poc.atp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncThreadlocalPropagationPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
